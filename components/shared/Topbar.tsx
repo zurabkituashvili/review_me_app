e0e815e22f52439ac6eb4408a77fb37aa84805c8
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-09-03 20:18:21 +0400

    first commit

---
+++ components/shared/Topbar.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { dark } from "@clerk/themes";
+import { usePathname, useRouter } from "next/navigation";
+
+function Topbar() {
+  const router = useRouter();
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap4">
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">TitCock</p>
+      </Link>
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton signOutCallback={() => router.push("/sign-in")}>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher
+          appearance={{
+            baseTheme: dark,
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
+}
+export default Topbar;
