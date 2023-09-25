59ed344be0ab0db00cc9334222e501cc0e7185d3
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-09-25 15:00:02 +0400

    done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -26,7 +26,6 @@ async function Home({
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
-      <Searchbar routeType="/" />
 
       <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
